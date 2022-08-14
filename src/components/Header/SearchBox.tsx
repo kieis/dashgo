1c0a62a75acedb485df6926fa7c901bac516a43a
Dirlan Ferreira <dirlan2014@yahoo.com.br>
2022-08-13 21:48:05 -0300

    added react-query & miragejs

--- src/components/Header/SearchBox.tsx
+++ src/components/Header/SearchBox.tsx
@@ -1,9 +1,9 @@
 import { Flex, Icon, Input } from "@chakra-ui/react";
-import { useRef, useState } from "react";
+import { useRef } from "react";
 import { RiSearchLine } from "react-icons/ri";
 
 export function SearchBox() {
-    const searchInputValue = useRef<HTMLInputElement>(null);
+    const searchInputValue = useRef<HTMLInputElement>(null); //uncontrolled component
 
     return (
         <Flex as="label" flex="1" py="4" px="8" ml="6" maxWidth={400} alignSelf="center" color="gray.200" position="relative" bg="gray.800" borderRadius="full">  
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,11 +1,35 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, useBreakpointValue, Spinner } from "@chakra-ui/react";
 import Link from "next/link";
+import { useEffect } from "react";
 import { RiAddLine } from "react-icons/ri";
+import { useQuery } from "react-query";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+    const { data, isLoading, error } = useQuery('users', async () => {
+        const response = await fetch('/api/users');
+        const data = await response.json();
+
+        const users = data.users.map((user) => {
+            return {
+                id: user.id,
+                name: user.name,
+                email: user.email,
+                createdAt: new Date(user.createdAt).toLocaleDateString('pt-br', {
+                    day: '2-digit',
+                    month: 'long',
+                    year: 'numeric'
+                })
+            };
+        });
+
+        return users;
+    }, {
+        staleTime: 1800 * 5 // 5 seconds as fresh
+    });
+
     const isWideVersion = useBreakpointValue({base: false, lg: true});
 
     return (
@@ -26,34 +50,50 @@ export default function UserList() {
                         </Link>
                     </Flex>
 
-                    <Table colorScheme="whiteAlpha">
-                        <Thead>
-                            <Tr>
-                                <Th px={["4", "4","6"]} color="gray.300" w="8">
-                                    <Checkbox colorScheme="pink"></Checkbox>
-                                </Th>
-                                <Th>Usuário</Th>
-                                { isWideVersion && <Th>Data de cadastro</Th>}
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <Td px={["4", "4","6"]}>
-                                    <Checkbox colorScheme="pink"></Checkbox>
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Dirlan Ferreira</Text>
-                                        <Text fontSize="sm" color="gray.300">dirlan20191@gmail.com</Text>
-                                    </Box>
-                                </Td>
-                                { isWideVersion &&<Td>
-                                    04 de Abril 2021
-                                </Td>}
-                            </Tr>
-                        </Tbody>
-                    </Table>
-                    <Pagination />
+                    { isLoading ? (
+                        <Flex justify="center">
+                            <Spinner />
+                        </Flex>
+                    ): error ? (
+                        <Text>Falha ao obter dados dos usuários.</Text>
+                    ) : (
+                        <>
+                            <Table colorScheme="whiteAlpha">
+                                <Thead>
+                                    <Tr>
+                                        <Th px={["4", "4","6"]} color="gray.300" w="8">
+                                            <Checkbox colorScheme="pink"></Checkbox>
+                                        </Th>
+                                        <Th>Usuário</Th>
+                                        { isWideVersion && <Th>Data de cadastro</Th>}
+                                    </Tr>
+                                </Thead>
+                                <Tbody>
+                                    {
+                                        data.map((user) => {
+                                            return (
+                                            <Tr key={user.id}>
+                                                <Td px={["4", "4","6"]}>
+                                                    <Checkbox colorScheme="pink"></Checkbox>
+                                                </Td>
+                                                <Td>
+                                                    <Box>
+                                                        <Text fontWeight="bold">{user.name}</Text>
+                                                        <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                                                    </Box>
+                                                </Td>
+                                                { isWideVersion &&<Td>
+                                                    { user.createdAt }
+                                                </Td>}
+                                            </Tr>
+                                            )
+                                        })
+                                    }
+                                </Tbody>
+                            </Table>
+                            <Pagination />
+                        </>
+                    )}
                 </Box>
             </Flex>
         </Box>
